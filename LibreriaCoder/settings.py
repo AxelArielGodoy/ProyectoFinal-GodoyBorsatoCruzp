614b85eab7cccfebec33405968a304b02753fda4
Victoria Cruz Pintor <victoriacruzpintor@outlook.com>
2022-07-27 11:36:48 +0200

    Version 017

--- LibreriaCoder/settings.py
+++ LibreriaCoder/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'LibreriaCoder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [r'C:\Users\Natu\ProyectoFinal-GodoyBorsatoCruzp\ProyectoFinal-GodoyBorsatoCruzp\templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
